25bd730e24649a185a6b99784627375e2c36117a
alpha9av <alpha9coder@gmail.com>
2025-06-28 12:32:50 +0530

    my comment

--- Backend/Middlewares/Auth.js
+++ Backend/Middlewares/Auth.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 const verifyToken = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
+    // hello 
     
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return res.status(401).json({
